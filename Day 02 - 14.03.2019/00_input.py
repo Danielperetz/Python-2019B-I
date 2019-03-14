0c3d475674209a2001685dc1b2dad77f24243630
annakarpf <anakarpf6@gmail.com>
2019-03-14 11:18:27 +0200

    python

---
+++ Day 02 - 14.03.2019/00_input.py
@@ -0,0 +1,19 @@
+
+# input:
+# Read a string from standard input. The trailing newline is stripped.
+# The prompt string, if given, is printed to standard output before reading input.
+
+# print
+# Prints the values to a stream, or to sys.stdout by default.
+
+age = input("Enter your age: ")
+print("your age is",age)
+
+
+"""
+Example of program execution:
+___________________________________
+
+Enter your age: 22
+your age is 22
+"""
---
+++ Day 02 - 14.03.2019/01_input.py
@@ -0,0 +1,14 @@
+
+age=input("Enter your age: ")  # input returns string
+print(age*3)
+age=int(age)        # parse string to int
+print(age*3)
+
+"""
+Example of program execution:
+___________________________________
+
+Enter your age: 5
+555
+15
+"""
